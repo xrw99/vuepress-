30ccc337040c6dcfdf1a17178891e619ddd2b519
coderxrw <1515789103@qq.com>
2020-08-27 10:07:09 +0800

    base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
     description: '我的心路历程',
     dest: './dist',
     port: '7777',
+    base:'/vuepress-blog/',
     head: [
         ['link', { rel: 'icon', href: '/favicon.ico' }]
     ],
